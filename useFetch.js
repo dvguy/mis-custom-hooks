b3417747ff3c74675253c90992e79219d25ef19a
dvguy <fernando.santoyo.dv@gmail.com>
2020-11-05 21:53:40 -0600

    primera foto

---
+++ useFetch.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react"
+
+export const useFetch = (url) => {
+    const [state, setstate] = useState({data: null, loading: true, error: null});
+
+    const isMounted = useRef(true);
+
+    useEffect(()=>{
+        
+        return () => {
+            isMounted.current = false;
+        }
+
+    }, []);
+
+    useEffect(() =>{
+        setstate({data: null, loading: true, error: null})
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            if(isMounted.current){
+                setstate({
+                    error: null,
+                    loading: false,
+                    data
+                });
+            }
+        });
+    },[url]);
+
+    return state;
+}
